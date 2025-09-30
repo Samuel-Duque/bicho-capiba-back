93539e3a7a8398883c3a1583b4414fa28fb37716
Samuel Locatel <samuellocatel@gmail.com>
2025-09-30 22:03:06 +0000

    feat(migrations): update users table schema and add animals table with attributes

---
+++ database/migrations/1759236545740_create_animals_table.ts
@@ -0,0 +1,36 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'animals'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').notNullable()
+      table.string('uuid').notNullable().unique()
+      table.string('nome').notNullable()
+      table.integer('idade').notNullable()
+      table.enum('sexo', ['M', 'F']).notNullable()
+      table.enum('porte', ['Pequeno', 'Medio', 'Grande']).notNullable()
+      table.string('cor').notNullable()
+      table.string('especie').notNullable()
+      table.string('raca').notNullable()
+      table.string('data_nascimento').nullable()
+      table.string('vacinas').nullable()
+      table.boolean('castrado').nullable()
+      table.string('necessidades_especiais').nullable()
+      table.text('historia').nullable()
+      table.enum('status_animal', ['Disponivel', 'Adotado', 'Pendente']).notNullable()
+      table.boolean('sociavel_animal').nullable()
+      table.boolean('sociavel_pessoa').nullable()
+      table.integer('ong_id')
+
+      table.timestamp('created_at').notNullable()
+      table.timestamp('updated_at').notNullable()
+      table.timestamp('deleted_at').nullable()
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
